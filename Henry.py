6071556319b0bc068a743e46c1f9bd0b5d0684a3
Indoors420 <38793945+Indoors420@users.noreply.github.com>
2018-08-14 21:06:11 -0700

    Update Henry.py

--- Henry.py
+++ Henry.py
@@ -12,25 +12,25 @@ async def on_ready():
         await bot.send_typing(GOAT.get_channel(os.getenv('GOAT_GENERAL')))
         await bot.send_typing(SUBPARMETA.get_channel(os.getenv('SUBPARMETA_HENRYS_CHANNEL')))
         await bot.send_typing(HENRYSSERVER.get_channel(os.getenv('HENRYSSERVER_ROBOT_NIGGAS')))
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         await bot.send_message(GOAT.get_channel(os.getenv('GOAT_GENERAL')), msg)
         await bot.send_message(SUBPARMETA.get_channel(os.getenv('SUBPARMETA_HENRYS_CHANNEL')), msg)
         await bot.send_message(HENRYSSERVER.get_channel(os.getenv('HENRYSSERVER_ROBOT_NIGGAS')), msg)
         msg = shitpost()
         await bot.send_typing(HENRYSSERVER.get_channel(os.getenv('HENRYSSERVER_INTEGRATION')))     
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         await bot.send_message(HENRYSSERVER.get_channel(os.getenv('HENRYSSERVER_INTEGRATION')), msg)  
         print("Meme Sent")
         print("Waiting "+str(seconds)+" seconds...")
         for _ in range(0,seconds):
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.8)
 @bot.event
 async def on_command_error(error: Exception, ctx: commands.Context):
     ignored = (commands.CommandNotFound, commands.UserInputError)
     error = getattr(error, 'original', error)
     if isinstance(error, ignored):
         await bot.send_typing(ctx.message.channel)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         msg = Lists.commandError[random.randint(0,len(Lists.commandError)-1)]
         await bot.send_message(ctx.message.channel, msg)
         return
@@ -42,7 +42,7 @@ async def on_message(message): #Handles responding to messages
     global counter
     if ("Henry, help" in message.content):
         await bot.send_typing(message.channel)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         msg = Lists.rejected[random.randint(0,len(Lists.rejected)-1)]
         await bot.send_message(message.channel, msg)
         return
@@ -70,34 +70,34 @@ async def on_message(message): #Handles responding to messages
             else:
                 msg = retaliate(2).format(message)
             await bot.send_typing(message.channel)
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.8)
             await bot.send_message(message.channel, msg)   
 @bot.command(pass_context = True)
 async def clear(ctx, input):
     if (ctx.message.author.server_permissions.manage_messages == False):
         msg = Lists.noRights[random.randint(0, len(Lists.noRights)-1)]
         await bot.send_typing(ctx.message.channel)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         await bot.send_message(ctx.message.channel, msg)
         return
     elif (ctx.message.channel.server.me.server_permissions.manage_messages == False):
         msg = Lists.botOutrank[random.randint(0, len(Lists.botOutrank)-1)]
         await bot.send_typing(ctx.message.channel)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         await bot.send_message(ctx.message.channel, msg)
         return
     else:
         if (not input.isdigit()):
             msg = Lists.badArg[random.randint(0, len(Lists.badArg)-1)]
             await bot.send_typing(ctx.message.channel)
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.8)
             await bot.send_message(ctx.message.channel, msg)
             return          
         input = int(input)
         if (input < 2):
             msg = Lists.badArg[random.randint(0, len(Lists.badArg)-1)]
             await bot.send_typing(ctx.message.channel)
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.8)
             await bot.send_message(ctx.message.channel, msg)
             return
         elif (input <= 100): #Command can clear from 2 to 100 messages by default
@@ -119,11 +119,11 @@ async def clear(ctx, input):
                     mgs.append(x)
                 if (len(mgs) > 0): #Don't try to delete messages that don't exist
                     await bot.delete_messages(mgs)
-                    await asyncio.sleep(1)
+                    await asyncio.sleep(0.8)
             remain = [] #Empty list to put all the messages in the log
             async for r in bot.logs_from(ctx.message.channel, limit = remainder):
                 remain.append(r)
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.8)
             if (len(remain) > 0): #Don't try to delete messages that don't exist
                 await bot.delete_messages(remain)
             else:
@@ -131,24 +131,24 @@ async def clear(ctx, input):
         elif(input >= 1000):
             msg = Lists.clear1k[random.randint(0, len(Lists.clear1k)-1)]
             await bot.send_typing(ctx.message.channel)
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.8)
             await bot.send_message(ctx.message.channel, msg)
 @bot.command(pass_context = True)
 async def kick(ctx, user: discord.Member):
     if (ctx.message.author.server_permissions.kick_members == False or user.id == "187656701380526080"):
         msg = Lists.noRights[random.randint(0, len(Lists.noRights)-1)]
         await bot.send_typing(ctx.message.channel)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         await bot.send_message(ctx.message.channel, msg)
     elif(ctx.message.server.me.top_role <= user.top_role):
         msg = Lists.botOutrank[random.randint(0, len(Lists.botOutrank)-1)]
         await bot.send_typing(ctx.message.channel)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         await bot.send_message(ctx.message.channel, msg)
     elif(ctx.message.author.top_role <= user.top_role):
         msg = Lists.authorOutrank[random.randint(0, len(Lists.authorOutrank)-1)]
         await bot.send_typing(ctx.message.channel)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.8)
         await bot.send_message(ctx.message.channel, msg)
     else:
         await bot.say('Okay {}, time to go.'.format(user.mention))
