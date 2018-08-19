abd63811186cd6256ea8dd9e50356c6a644884d9
Craig McIlwrath <craigmc08@gmail.com>
2018-08-19 14:09:13 -0400

    Add ConversationManager, to manage conversations (#6)

---
+++ ConversationManager.py
@@ -0,0 +1,42 @@
+# Usage:
+#   Create a conversation with `add`
+#   Check if a conversation exists with `is_conversing_with`
+#   To tell the system that a message has ben responded with, call `update`
+
+import time, discord
+
+
+class ConversationManager:
+    conversations = []
+    conversationTimes = {}
+    timeout = 8.0
+
+    def __init__(self, timeout=8.0):
+        self.timeout = timeout
+
+    def remove_stale_conversations(self) -> None:
+        for user_id, conversationTime in self.conversationTimes.items():
+            if time.time() - conversationTime> self.timeout:
+                self.conversations.remove(user_id)
+                self.conversationTimes[user_id] = None
+        self.conversationTimes = {k: v for k, v in self.conversationTimes.items() if v is not None}
+
+    def add(self, user: discord.User) -> None:
+        self.remove_stale_conversations()
+        if user.id not in self.conversations:
+            self.conversations.append(user.id)
+            self.conversationTimes[user.id] = time.time()
+        else:
+            self.conversationTimes[user.id] = time.time()
+
+    def remove(self, user: discord.User) -> None:
+        self.remove_stale_conversations()
+        if user.id in self.conversations: self.conversations.remove(user.id)
+
+    def conversing_with(self, user: discord.User) -> bool:
+        self.remove_stale_conversations()
+        return user.id in self.conversations
+
+    def update(self, user: discord.User) -> None:
+        if user.id in self.conversations: self.conversationTimes[user.id] = time.time()
+        self.remove_stale_conversations()
