347b7bdfef544fc15413f040b3df1ff3930a5e6a
Indoors420 <38793945+Indoors420@users.noreply.github.com>
2018-08-18 22:10:57 -0700

    Add files via upload

--- Henry.py
+++ Henry.py
@@ -41,9 +41,9 @@ async def on_message(message): #Handles responding to messages
                 await asyncio.sleep(0.8)
                 await bot.send_message(message.channel, msg)
                 response = await bot.wait_for_message(author=message.author, timeout = 8.0)
-                print('response: '+response.content)
         else:
             while (response != None):
+                print('response: '+response.content)
                 conversing = True
                 print('conversing: '+str(conversing))
                 msg = msgGen(lMessage, 1)
