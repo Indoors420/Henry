6777aae901dda37dbdb814983eecd4eab737ad1f
Indoors420 <38793945+Indoors420@users.noreply.github.com>
2018-08-18 18:58:51 -0700

    Add files via upload

--- Henry.py
+++ Henry.py
@@ -38,7 +38,7 @@ async def on_message(message): #Handles responding to messages
             await asyncio.sleep(0.8)
             await bot.send_message(message.channel, msg)
             response = await bot.wait_for_message(author=message.author, timeout = 5.0)
-            print(response)
+            print(response.content)
 @bot.command(pass_context = True)
 async def clear(ctx, input):
     if (ctx.message.author.server_permissions.manage_messages == False):
